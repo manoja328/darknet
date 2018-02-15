3aa1d36d72cc3cb26c0a9da558d44f846596e54f
manoja328 <manoja328@gmail.com>
2018-02-14 19:21:47 -0500

    add epochs saving to 1000 and checkpionint restart

--- trainxview.sh
+++ trainxview.sh
@@ -1,1 +1,4 @@
-./darknet detector train data/xview.data cfg/yolo-xview.cfg darknet19_448.conv.23 -gpus 0
+#./darknet detector train data/xview.data cfg/yolo-xview.cfg darknet19_448.conv.23 -gpus 0
+#./darknet detector train data/xview.data cfg/yolo-xview.cfg backup/yolo -gpus 0
+
+./darknet detector train data/xview.data cfg/yolo-xview.cfg backup/yolo-xview.backup -gpus 0
